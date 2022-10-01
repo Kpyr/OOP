3df316f11b4fc31f2542341809c5d88e5558b797
Kpyr <igrovaaplaneta@gmail.com>
2022-10-01 19:54:51 +0300

    Чистка приколов

---
+++ cmake-build-debug/CMakeFiles/OOP_Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_Lab1.dir/main.cpp.obj"
+  "CMakeFiles/OOP_Lab1.dir/main.cpp.obj.d"
+  "OOP_Lab1.exe"
+  "OOP_Lab1.exe.manifest"
+  "OOP_Lab1.lib"
+  "OOP_Lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
