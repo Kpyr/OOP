62311b19121838cd56f5227bf053acc1e65a9df2
Kpyr <igrovaaplaneta@gmail.com>
2022-10-01 20:57:19 +0300

    VS

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 int main() {
   ///Тест слияния и проверка апдейта версии
+	 ///Тест слияния и проверка апдейта версии
   cout << "Hello, World!" << endl;
   cout << "Lb2!";
   return 0;
