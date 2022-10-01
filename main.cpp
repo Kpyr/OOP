689578cc283fc3f4d060d2c0e0d2aed81d0bf2ed
Kpyr <igrovaaplaneta@gmail.com>
2022-10-01 20:23:01 +0300

    тест

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-//#include <vld.h>
 #include <iostream>
 #include <cmath>
 #include <Windows.h>
 using namespace std;
 int main() {
+  ///Тест слияния и проверка апдейта версии
   cout << "Hello, World!" << endl;
   cout << "Lb2!";
   return 0;
