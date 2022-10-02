6a1c52b6b093ce396aea2c2b19f93e6366b53d62
Skulllalka <skulllalka@gmail.com>
2022-10-02 23:26:53 +0300

    Last vers

--- main.cpp
+++ main.cpp
@@ -6,17 +6,21 @@ class Body {
 
  public:
   double V = 0, S = 0, num_PI = 3.14;
-  virtual void V_find(){};
-  virtual void S_find(){};
+  virtual double V_find(double temp1,double temp2){
+      return 0;
+  };
+  virtual double S_find(double temp1, double temp2, double temp3){
+      return 0;
+  };
 };
 
 class Parallelepiped : public Body {
  public:
-  virtual double V_find(double length, double width, double height) {
+  double V_find(double length, double width, double height) {
     V = length * width * height;
     return V;
   }
-  virtual double S_find(double length, double width, double height) {
+  double S_find(double length, double width, double height) {
     S = 2 * (length * width + length * height + width * height);
     return S;
   }
@@ -24,12 +28,11 @@ class Parallelepiped : public Body {
 
 class Cone : public Body {
  public:
-  virtual double V_find(double H, double R) {
+  double V_find(double H, double R) {
     V =(num_PI*R*R*H)/3;
-    cout << V;
     return V;
   }
-  virtual double S_find(double L, double R) {
+  double S_find(double L, double R) {
     S = (num_PI * R * L) + (num_PI * R * R);
     return S;
   }
@@ -38,54 +41,54 @@ class Cone : public Body {
 
 class Ball : public Body {
  public:
-  virtual double V_find(double R) {
+  double V_find(double R) {
     V = (num_PI * R * R * R*4) / 3;
     return V;
   }
-  virtual double S_find(double R) {
+  double S_find(double R) {
     S = 4*num_PI*R*R;
     return S;
   }
 };
-/*class Employee {
-public:
-	virtual void virt() {
-		cout << "EMP" << endl;
-	}
-};
-class Manager : public Employee {
-public:
-	void virt() {
-		cout << "MAN" << endl;
-	}
-};
-class Rob : public Employee {
-public:
-	void virt() {
-		cout << "Rob" << endl;
-	}
-};*/
+//class Employee {
+//public:
+//	virtual void virt() {
+//		cout << "EMP" << endl;
+//	}
+//};
+//class Manager : public Employee {
+//public:
+//	void virt() {
+//		cout << "MAN" << endl;
+//	}
+//};
+//class Rob : public Employee {
+//public:
+//	void virt() {
+//		cout << "Rob" << endl;
+//		cout << "Privet";
+//	}
+//};
 int main() {
   setlocale(LC_ALL, "eng");
-/*	Body* list[3];
+	Body* list[3];
 	Cone* conus = new Cone;
 	Ball* shar = new Ball;
 	Parallelepiped* pram = new Parallelepiped;
 	list[0] = pram; list[1] = conus; list[2] = shar;
 	double temp1, temp2, temp3;
-	cin >> temp1 >> temp2 >> temp3;*/
-  //double ans = list[0]->V_find(temp1, temp2, temp3);
-
-
-  Cone* ptr = new Cone;
-  ptr->V_find(1,2);
+	cin >> temp1 >> temp2 >> temp3;
+    double ans = list[1]->V_find(temp1,temp2);
+    cout << ans;
+ 
+  
 
   /*Cone cn1;
   cn1.V_find(1,2);*/
-  /*Employee* list[2];
+ /* Employee* list[2];
   Rob* ro = new Rob;
   Manager* man = new Manager;
   list[0] = ro; list[1] = man;
-  for (int i = 0; i < 2; i++) list[i]->virt();
-      return 0;*/
+  for (int i = 0; i < 2; i++) list[i]->virt();*/
+      return 0;
 }
