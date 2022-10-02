5a3cf4fbe40d4ffed55a5a260d4a1d5d42c97c05
Skulllalka <skulllalka@gmail.com>
2022-10-02 22:38:14 +0300

    new

--- main.cpp
+++ main.cpp
@@ -1,19 +1,84 @@
 #include <iostream>
 #include <cmath>
 #include <Windows.h>
-#include <vector>
 using namespace std;
+//class Body {
+//	
+//public:
+//	double V = 0, S = 0, num_PI = 3.14;
+//	virtual double V_find(double temp1,double temp2, double temp3);
+//	virtual double S_find();
+//};
+//
+//class Parallelepiped : public Body {
+//public:
+//	double V_find(double length, double width, double height) {
+//		V = length * width * height;
+//		return V;
+//	}
+//	double S_find(double length, double width, double height) {
+//		S = 2 * (length * width + length * height + width * height);
+//		return S;
+//	}
+//};
+//
+//class Cone : public Body {
+//public:
+//	double V_find(double H, double R) {
+//		V =(num_PI*R*R*H)/3;
+//		return V;
+//	}
+//	double S_find(double L, double R) {
+//		S = (num_PI * R * L) + (num_PI * R * R);
+//		return S;
+//	}
+//
+//};
+//
+//class Ball : public Body {
+//public:
+//	double V_find(double R) {
+//		V = (num_PI * R * R * R*4) / 3;
+//		return V;
+//	}
+//	double S_find(double R) {
+//		S = 4*num_PI*R*R;
+//		return S;
+//	}
+//};
+class Employee {
+public:
+	virtual void virt() {
+		cout << "EMP" << endl;
+	}
+};
+class Manager : public Employee {
+public:
+	void virt() {
+		cout << "MAN" << endl;
+	}
+};
+class Rob : public Employee {
+public:
+	void virt() {
+		cout << "Rob" << endl;
+	}
+};
 int main() {
-  ///Тест слияния и проверка апдейта версии
-  ///Тест update from VS
-  cout << "Hello, World!" << endl;
-  cout << "Lb2!";
-  cout << "lol";
-
-
-  /// <summary>
-  /// 
-  /// </summary>
-  /// <returns></returns>
-  return 0;
+	//setlocale(LC_ALL, "rus");
+	//Body* list[3];
+	//Cone* conus = new Cone;
+	////Ball* shar = new Ball;
+	////Parallelepiped* pram = new Parallelepiped;
+	////list[0] = pram; list[1] = conus; list[2] = shar;
+	//double temp1, temp2, temp3;
+	//cin >> temp1 >> temp2 >> temp3;
+	////double ans = list[0]->V_find(temp1, temp2, temp3);
+	Employee* list[2];
+	Rob* ro = new Rob;
+	Manager* man = new Manager;
+	list[0] = ro; list[1] = man;
+	for (int i = 0; i < 2; i++) list[i]->virt();
+	>>>>>> > Считай только начало, нужно пофиксить ошибки, а так в целом должно быть норм.
+		return 0;
 }
