20727182a8ab687ff47d6b2de1ca405347307b52
Kpyr <igrovaaplaneta@gmail.com>
2022-10-02 23:04:54 +0300

    V3

--- main.cpp
+++ main.cpp
@@ -2,51 +2,52 @@
 #include <cmath>
 #include <Windows.h>
 using namespace std;
-//class Body {
-//	
-//public:
-//	double V = 0, S = 0, num_PI = 3.14;
-//	virtual double V_find(double temp1,double temp2, double temp3);
-//	virtual double S_find();
-//};
-//
-//class Parallelepiped : public Body {
-//public:
-//	double V_find(double length, double width, double height) {
-//		V = length * width * height;
-//		return V;
-//	}
-//	double S_find(double length, double width, double height) {
-//		S = 2 * (length * width + length * height + width * height);
-//		return S;
-//	}
-//};
-//
-//class Cone : public Body {
-//public:
-//	double V_find(double H, double R) {
-//		V =(num_PI*R*R*H)/3;
-//		return V;
-//	}
-//	double S_find(double L, double R) {
-//		S = (num_PI * R * L) + (num_PI * R * R);
-//		return S;
-//	}
-//
-//};
-//
-//class Ball : public Body {
-//public:
-//	double V_find(double R) {
-//		V = (num_PI * R * R * R*4) / 3;
-//		return V;
-//	}
-//	double S_find(double R) {
-//		S = 4*num_PI*R*R;
-//		return S;
-//	}
-//};
-class Employee {
+class Body {
+
+ public:
+  double V = 0, S = 0, num_PI = 3.14;
+  virtual void V_find(){};
+  virtual void S_find(){};
+};
+
+class Parallelepiped : public Body {
+ public:
+  virtual double V_find(double length, double width, double height) {
+    V = length * width * height;
+    return V;
+  }
+  virtual double S_find(double length, double width, double height) {
+    S = 2 * (length * width + length * height + width * height);
+    return S;
+  }
+};
+
+class Cone : public Body {
+ public:
+  virtual double V_find(double H, double R) {
+    V =(num_PI*R*R*H)/3;
+    cout << V;
+    return V;
+  }
+  virtual double S_find(double L, double R) {
+    S = (num_PI * R * L) + (num_PI * R * R);
+    return S;
+  }
+
+};
+
+class Ball : public Body {
+ public:
+  virtual double V_find(double R) {
+    V = (num_PI * R * R * R*4) / 3;
+    return V;
+  }
+  virtual double S_find(double R) {
+    S = 4*num_PI*R*R;
+    return S;
+  }
+};
+/*class Employee {
 public:
 	virtual void virt() {
 		cout << "EMP" << endl;
@@ -63,22 +64,28 @@ public:
 	void virt() {
 		cout << "Rob" << endl;
 	}
-};
+};*/
 int main() {
-	//setlocale(LC_ALL, "rus");
-	//Body* list[3];
-	//Cone* conus = new Cone;
-	////Ball* shar = new Ball;
-	////Parallelepiped* pram = new Parallelepiped;
-	////list[0] = pram; list[1] = conus; list[2] = shar;
-	//double temp1, temp2, temp3;
-	//cin >> temp1 >> temp2 >> temp3;
-	////double ans = list[0]->V_find(temp1, temp2, temp3);
-	Employee* list[2];
-	Rob* ro = new Rob;
-	Manager* man = new Manager;
-	list[0] = ro; list[1] = man;
-	for (int i = 0; i < 2; i++) list[i]->virt();
-	>>>>>> > —читай только начало, нужно пофиксить ошибки, а так в целом должно быть норм.
-		return 0;
+  setlocale(LC_ALL, "eng");
+/*	Body* list[3];
+	Cone* conus = new Cone;
+	Ball* shar = new Ball;
+	Parallelepiped* pram = new Parallelepiped;
+	list[0] = pram; list[1] = conus; list[2] = shar;
+	double temp1, temp2, temp3;
+	cin >> temp1 >> temp2 >> temp3;*/
+  //double ans = list[0]->V_find(temp1, temp2, temp3);
+
+
+  Cone* ptr = new Cone;
+  ptr->V_find(1,2);
+
+  /*Cone cn1;
+  cn1.V_find(1,2);*/
+  /*Employee* list[2];
+  Rob* ro = new Rob;
+  Manager* man = new Manager;
+  list[0] = ro; list[1] = man;
+  for (int i = 0; i < 2; i++) list[i]->virt();
+      return 0;*/
 }
