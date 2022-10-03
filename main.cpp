c3bf3148ae8e8376c75871e6b6b79056e5ed7604
Skulllalka <skulllalka@gmail.com>
2022-10-03 11:05:51 +0300

    Last vers, можно на всякий пересмотреть если что  - то не так будет, но вроде нормально работает.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
+//--------------------------------------------------------
+// ИКТИБ ЮФУ
+// Кафедра МОП ЭВМ
+// ООП
+// Лабораторная работа №2, варант 4
+// КТбо2-8 
+// Степочкин Михаил Андреевич
+// Кузьменко Кирилл Сергеевич
+// 03.10.2022
+//--------------------------------------------------------
 #include <iostream>
 #include <cmath>
 #include <Windows.h>
 using namespace std;
 class Body {
-
+protected:
+    double V = 0, S = 0, num_PI = 3.14;
  public:
-  double V = 0, S = 0, num_PI = 3.14;
+  
   virtual double V_find(double *arr){
       return 0;
   };
   virtual double S_find(double *arr){
       return 0;
   };
+  ~Body() {};
 };
 
 class Parallelepiped : public Body {
@@ -28,12 +40,12 @@ class Parallelepiped : public Body {
 
 class Cone : public Body {
  public:
-  double V_find(double H, double R) {
-    V =(num_PI*R*R*H)/3;
+  double V_find(double* arr) {
+    V =(num_PI*arr[0] * arr[0] * arr[1]) / 3;
     return V;
   }
-  double S_find(double L, double R) {
-    S = (num_PI * R * L) + (num_PI * R * R);
+  double S_find(double* arr) {
+    S = (num_PI * arr[0] * arr[1]) + (num_PI * arr[0] * arr[0]);
     return S;
   }
 
@@ -41,56 +53,108 @@ class Cone : public Body {
 
 class Ball : public Body {
  public:
-  double V_find(double R) {
-    V = (num_PI * R * R * R*4) / 3;
+  double V_find(double* arr) {
+    V = (num_PI * arr[0] * arr[0] * arr[0] * 4) / 3;
     return V;
   }
-  double S_find(double R) {
-    S = 4*num_PI*R*R;
+  double S_find(double* arr) {
+    S = 4*num_PI* arr[0] * arr[0];
     return S;
   }
 };
-//class Employee {
-//public:
-//	virtual void virt() {
-//		cout << "EMP" << endl;
-//	}
-//};
-//class Manager : public Employee {
-//public:
-//	void virt() {
-//		cout << "MAN" << endl;
-//	}
-//};
-//class Rob : public Employee {
-//public:
-//	void virt() {
-//		cout << "Rob" << endl;
-//		cout << "Privet";
-//	}
-//};
+
 int main() {
-  setlocale(LC_ALL, "eng");
+  setlocale(LC_ALL, "rus");
 	Body* list[3];
 	Cone* conus = new Cone;
 	Ball* shar = new Ball;
 	Parallelepiped* pram = new Parallelepiped;
 	list[0] = pram; list[1] = conus; list[2] = shar;
-	double temp1, temp2, temp3;
-	cin >> temp1 >> temp2 >> temp3;
-    double arr[3]{};
-    for (int i = 0; i < 3; i++) cin >> arr[i];
-    double ans = list[0]->V_find(arr);
-    cout << ans;
- 
-  
-
-  /*Cone cn1;
-  cn1.V_find(1,2);*/
- /* Employee* list[2];
-  Rob* ro = new Rob;
-  Manager* man = new Manager;
-  list[0] = ro; list[1] = man;
-  for (int i = 0; i < 2; i++) list[i]->virt();*/
+    bool chek = true;
+    while (chek) {
+        cout << "=================================================================" << endl;
+        cout << "Выберите фигуру, для которой необходимо выполнить вычисления:" << endl;
+        cout << "1) Прямоугольный параллелипипед" << endl;
+        cout << "2) Конус" << endl;
+        cout << "3) Шар" << endl;
+        short int choice;
+        cin >> choice;
+        cout << "=================================================================" << endl;
+        double arr[3]{};
+        double V = 0, S = 0;
+        short int math;
+        switch (choice){
+        case 1:
+            cout << "Для вычисления объёма фигуры введите 1, для вычиселния площади введите 2: " << endl;
+            cin >> math;
+            if (math == 1) {
+                cout << "Введите значения длины, ширины и высоты параллелипипида: " << endl;
+                for (int i = 0; i < 3; i++)cin >> arr[i];
+                V = list[0]->V_find(arr);
+                cout << "Объём фигуры равен = " << V << endl;
+            }
+            else {
+                cout << "Введите значения длины, ширины и высоты параллелипипида: " << endl;
+                for (int i = 0; i < 3; i++)cin >> arr[i];
+                S = list[0]->S_find(arr);
+				cout << "Площадь фигуры равна = " << S << endl;
+            }
+            break;
+        case 2:
+            cout << "Для вычисления объёма фигуры введите 1, для вычиселния площади введите 2: " << endl;
+            cin >> math;
+            if (math == 1) {
+                cout << "Введите значения радиуса основания и высоты конуса: " << endl;
+                for (int i = 0; i < 2; i++)cin >> arr[i];
+                V = list[1]->V_find(arr);
+                cout << "Объём фигуры равен = " << V << endl;
+            }
+            else {
+                cout << "Введите значения радиуса основания и длины образующей: "<< endl;
+                for (int i = 0; i < 2; i++)cin >> arr[i];
+                S = list[1]->S_find(arr);
+                cout << "Площадь фигуры равна = " << S << endl;
+            }
+            break;
+        case 3:
+            cout << "Для вычисления объёма фигуры введите 1, для вычиселния площади введите 2: " << endl;
+            cin >> math;
+            if (math == 1) {
+                cout << "Введите значения радиуса фигуры: " << endl;
+                for (int i = 0; i < 1; i++)cin >> arr[i];
+                V = list[2]->V_find(arr);
+                cout << "Объём фигуры равен = " << V << endl;
+            }
+            else {
+                cout << "Введите значения радиуса фигуры: " << endl;
+                for (int i = 0; i < 1; i++)cin >> arr[i];
+                S = list[2]->S_find(arr);
+                cout << "Площадь фигуры равна = " << S << endl;
+            }
+            break;
+        default:
+            cout << "Неизвестная команда!" << endl;
+            break;
+        }
+        string str;
+        bool flg = true;
+        do {
+            cout << "Введите 'end' что бы завершить программу или 'nxt', что бы повторить запрос." << endl;
+            cin >> str;
+            {
+                if (str == "end") {
+                    chek = false;
+                    flg = false;
+                }
+                else if (str == "nxt") {
+                    flg = false;
+                }
+                else {
+                    cout << "Неизвестная команда, повторите попытку!" << endl;
+                }
+            }
+        } while (flg);
+    }
+    delete pram, shar, shar, list;
       return 0;
 }
