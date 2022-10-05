b3bb97f88351da3e9284eb409147a458d9fb171d
Kpyr <igrovaaplaneta@gmail.com>
2022-10-05 19:04:12 +0300

    UPD: - Add comm - Add example how struct/class work

--- main.cpp
+++ main.cpp
@@ -24,13 +24,70 @@
 using namespace std;
 int main() {
   system("chcp 65001");
-  long msDate[3];
+
+
+
+  //---------------------------------Struct module-----------------------------------------
+
+  long dateafter[3];
+  dateafter[0] = 1;
+  dateafter[1] = 1;
+  dateafter[2] = 2001;
+
+
+  employee* em1;
+  em1 = new employee;
+  em1->name = "Oleg";
+  em1->surname = "Borisenko";
+  em1->patronymic = "Olegovich";
+  em1->age = 34;
+  em1->expDay = 1;
+  em1->expMonth = 1;
+  em1->expYear = 2000;
+  em1->profession = "Clean";
+  em1->speciality = "Clean table";
+
+  showInfo(em1);
+
+  changeSurname(em1,"Glavenko");
+
+  showInfo(em1);
+
+  changeProfessionAndSpeciality(em1,"Boss", "Nothing");
+
+  showInfo(em1);
+
+  long *ms;
+  ms = calcExperience(em1, dateafter);
+  cout << "Стаж работы: " << endl;
+  cout << "Дней всего: " << ms[3] << endl;
+  cout << "Дней: " << ms[0] << endl << "Месяцов: " << ms[1] << endl << "Лет: " << ms[2] << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //---------------------------------Class module-----------------------------------------
+
+  /*long msDate[3];
   msDate[0] = 1;
   msDate[1] = 1;
   msDate[2] = 2000;
   employeeClass* emp1;
   //emp1 = new employeeClass;
-  emp1 = new employeeClass("Oleg","Borisenko","Olegovich",34,msDate,"Clean Master","Clean Touilet");
+  emp1 = new employeeClass("Oleg","Borisenko","Olegovich",34,msDate,"Clean","Clean table");
 
 
 
@@ -47,7 +104,7 @@ int main() {
   ms = emp1->calcExperience(dateafter);
   cout << "Стаж работы: " << endl;
   cout << "Дней всего: " << ms[3] << endl;
-  cout << "Дней: " << ms[0] << endl << "Месяцов: " << ms[1] << endl << "Лет: " << ms[2] << endl;
+  cout << "Дней: " << ms[0] << endl << "Месяцов: " << ms[1] << endl << "Лет: " << ms[2] << endl;*/
 
   return 0;
 }
