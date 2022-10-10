b3b164ff135d312ae9e197441ee6c2fb3659695c
Kpyr <igrovaaplaneta@gmail.com>
2022-10-11 01:09:29 +0300

    UPD:

--- main.cpp
+++ main.cpp
@@ -64,130 +64,114 @@ class Factory{
   }
 };
 
-class Operation:public Shape{
+class Operation:public Shape {
  public:
-  static long Compare(Shape* first, Shape* second){
+  static long Compare(Shape *first, Shape *second) {
     float first_S, second_S;
-    if(first->ID == "Triangle"){
-      float a,b,c,p;
-      a = sqrt(pow(first->arc[0].x - first->arc[1].x,2) + pow(first->arc[0].y - first->arc[1].y,2));
-      b = sqrt(pow(first->arc[1].x - first->arc[2].x,2) + pow(first->arc[1].y - first->arc[2].y,2));
-      c = sqrt(pow(first->arc[2].x - first->arc[0].x,2) + pow(first->arc[2].y - first->arc[0].y,2));
-      p = (a + b + c)/2;
+    if (first->ID == "Triangle") {
+      float a, b, c, p;
+      a = sqrt(pow(first->arc[0].x - first->arc[1].x, 2) + pow(first->arc[0].y - first->arc[1].y, 2));
+      b = sqrt(pow(first->arc[1].x - first->arc[2].x, 2) + pow(first->arc[1].y - first->arc[2].y, 2));
+      c = sqrt(pow(first->arc[2].x - first->arc[0].x, 2) + pow(first->arc[2].y - first->arc[0].y, 2));
+      p = (a + b + c) / 2;
 
       first_S = sqrt(p * (p - a) * (p - b) * (p - c));
-    }
-    else if(first->ID == "Trapeze"){
-      float a,b,c,d;
-      a = sqrt(pow(first->arc[0].x - first->arc[1].x,2) + pow(first->arc[0].y - first->arc[1].y,2));
-      b = sqrt(pow(first->arc[1].x - first->arc[2].x,2) + pow(first->arc[1].y - first->arc[2].y,2));
-      c = sqrt(pow(first->arc[2].x - first->arc[3].x,2) + pow(first->arc[2].y - first->arc[3].y,2));
-      d = sqrt(pow(first->arc[3].x - first->arc[0].x,2) + pow(first->arc[3].y - first->arc[0].y,2));
+    } else if (first->ID == "Trapeze") {
+      float a, b, c, d;
+      a = sqrt(pow(first->arc[0].x - first->arc[1].x, 2) + pow(first->arc[0].y - first->arc[1].y, 2));
+      b = sqrt(pow(first->arc[1].x - first->arc[2].x, 2) + pow(first->arc[1].y - first->arc[2].y, 2));
+      c = sqrt(pow(first->arc[2].x - first->arc[3].x, 2) + pow(first->arc[2].y - first->arc[3].y, 2));
+      d = sqrt(pow(first->arc[3].x - first->arc[0].x, 2) + pow(first->arc[3].y - first->arc[0].y, 2));
 
-      first_S = (((a+b)/2) * sqrt(pow(c,2) - pow((pow(b-a,2) + pow(c,2) - pow(d,2))/(2 * (b - a)),2)));
+      first_S = (((a + b) / 2) * sqrt(pow(c, 2) - pow((pow(b - a, 2) + pow(c, 2) - pow(d, 2)) / (2 * (b - a)), 2)));
       //cout << first_S << endl;
     }
 
-
-    if(second->ID == "Triangle"){
-      float a,b,c,p;
-      a = sqrt(pow(second->arc[0].x - second->arc[1].x,2) + pow(second->arc[0].y - second->arc[1].y,2));
-      b = sqrt(pow(second->arc[1].x - second->arc[2].x,2) + pow(second->arc[1].y - second->arc[2].y,2));
-      c = sqrt(pow(second->arc[2].x - second->arc[0].x,2) + pow(second->arc[2].y - second->arc[0].y,2));
-      p = (a + b + c)/2;
+    if (second->ID == "Triangle") {
+      float a, b, c, p;
+      a = sqrt(pow(second->arc[0].x - second->arc[1].x, 2) + pow(second->arc[0].y - second->arc[1].y, 2));
+      b = sqrt(pow(second->arc[1].x - second->arc[2].x, 2) + pow(second->arc[1].y - second->arc[2].y, 2));
+      c = sqrt(pow(second->arc[2].x - second->arc[0].x, 2) + pow(second->arc[2].y - second->arc[0].y, 2));
+      p = (a + b + c) / 2;
 
       second_S = sqrt(p * (p - a) * (p - b) * (p - c));
-    }
-    else if(second->ID == "Trapeze"){
-      float a,b,c,d;
-      a = sqrt(pow(second->arc[0].x - second->arc[1].x,2) + pow(second->arc[0].y - second->arc[1].y,2));
-      b = sqrt(pow(second->arc[1].x - second->arc[2].x,2) + pow(second->arc[1].y - second->arc[2].y,2));
-      c = sqrt(pow(second->arc[2].x - second->arc[3].x,2) + pow(second->arc[2].y - second->arc[3].y,2));
-      d = sqrt(pow(second->arc[3].x - second->arc[0].x,2) + pow(second->arc[3].y - second->arc[0].y,2));
+    } else if (second->ID == "Trapeze") {
+      float a, b, c, d;
+      a = sqrt(pow(second->arc[0].x - second->arc[1].x, 2) + pow(second->arc[0].y - second->arc[1].y, 2));
+      b = sqrt(pow(second->arc[1].x - second->arc[2].x, 2) + pow(second->arc[1].y - second->arc[2].y, 2));
+      c = sqrt(pow(second->arc[2].x - second->arc[3].x, 2) + pow(second->arc[2].y - second->arc[3].y, 2));
+      d = sqrt(pow(second->arc[3].x - second->arc[0].x, 2) + pow(second->arc[3].y - second->arc[0].y, 2));
 
-      second_S = (((a+b)/2) * sqrt(pow(c,2) - pow((pow(b-a,2) + pow(c,2) - pow(d,2))/(2 * (b - a)),2)));
+      second_S = (((a + b) / 2) * sqrt(pow(c, 2) - pow((pow(b - a, 2) + pow(c, 2) - pow(d, 2)) / (2 * (b - a)), 2)));
     }
 
-    if(first_S > second_S){
+    if (first_S > second_S) {
       return 1;
-    }
-    else if(first_S == second_S){
+    } else if (first_S == second_S) {
       return 2;
-    }
-    else{
+    } else {
       return 3;
     }
   }
 
-  static long isIntersect(Shape* first, Shape* second){
-    double first_havyX,first_havyY,second_havyX,second_havyY;
+  static long isIntersect(Shape *first, Shape *second) {
+    double first_havyX, first_havyY, second_havyX, second_havyY;
     double first_R, second_R;
-    double tmp1,tmp2,tmp3,tmp4;
+    double tmp1, tmp2, tmp3, tmp4;
 
-    if(first->ID == "Triangle"){
+    if (first->ID == "Triangle") {
       first_havyX = (first->arc[0].x + first->arc[1].x + first->arc[2].x) / 3;
       first_havyY = (first->arc[0].y + first->arc[1].y + first->arc[2].y) / 3;
 
-      tmp1 = sqrt(pow(first_havyX - first->arc[0].x,2) + pow(first_havyY - first->arc[0].y,2));
-      tmp2 = sqrt(pow(first_havyX - first->arc[1].x,2) + pow(first_havyY - first->arc[1].y,2));
-      tmp3 = sqrt(pow(first_havyX - first->arc[2].x,2) + pow(first_havyY - first->arc[2].y,2));
+      tmp1 = sqrt(pow(first_havyX - first->arc[0].x, 2) + pow(first_havyY - first->arc[0].y, 2));
+      tmp2 = sqrt(pow(first_havyX - first->arc[1].x, 2) + pow(first_havyY - first->arc[1].y, 2));
+      tmp3 = sqrt(pow(first_havyX - first->arc[2].x, 2) + pow(first_havyY - first->arc[2].y, 2));
 
-
-      first_R = max(tmp1,max(tmp2,tmp3));
-    }
-    else if(first->ID == "Trapeze"){
+      first_R = max(tmp1, max(tmp2, tmp3));
+    } else if (first->ID == "Trapeze") {
       first_havyX = (first->arc[0].x + first->arc[1].x + first->arc[2].x + first->arc[3].x) / 4;
       first_havyY = (first->arc[0].y + first->arc[1].y + first->arc[2].y + first->arc[3].y) / 4;
 
+      tmp1 = sqrt(pow(first_havyX - first->arc[0].x, 2) + pow(first_havyY - first->arc[0].y, 2));
+      tmp2 = sqrt(pow(first_havyX - first->arc[1].x, 2) + pow(first_havyY - first->arc[1].y, 2));
+      tmp3 = sqrt(pow(first_havyX - first->arc[2].x, 2) + pow(first_havyY - first->arc[2].y, 2));
+      tmp4 = sqrt(pow(first_havyX - first->arc[3].x, 2) + pow(first_havyY - first->arc[3].y, 2));
 
-      tmp1 = sqrt(pow(first_havyX - first->arc[0].x,2) + pow(first_havyY - first->arc[0].y,2));
-      tmp2 = sqrt(pow(first_havyX - first->arc[1].x,2) + pow(first_havyY - first->arc[1].y,2));
-      tmp3 = sqrt(pow(first_havyX - first->arc[2].x,2) + pow(first_havyY - first->arc[2].y,2));
-      tmp4 = sqrt(pow(first_havyX - first->arc[3].x,2) + pow(first_havyY - first->arc[3].y,2));
-
-
-      first_R = max(tmp1,max(tmp2,max(tmp3,tmp4)));
+      first_R = max(tmp1, max(tmp2, max(tmp3, tmp4)));
     }
 
-    if(second->ID == "Triangle"){
+    if (second->ID == "Triangle") {
       second_havyX = (second->arc[0].x + second->arc[1].x + second->arc[2].x) / 3;
       second_havyY = (second->arc[0].y + second->arc[1].y + second->arc[2].y) / 3;
 
-      tmp1 = sqrt(pow(second_havyX - second->arc[0].x,2) + pow(second_havyY - second->arc[0].y,2));
-      tmp2 = sqrt(pow(second_havyX - second->arc[1].x,2) + pow(second_havyY - second->arc[1].y,2));
-      tmp3 = sqrt(pow(second_havyX - second->arc[2].x,2) + pow(second_havyY - second->arc[2].y,2));
+      tmp1 = sqrt(pow(second_havyX - second->arc[0].x, 2) + pow(second_havyY - second->arc[0].y, 2));
+      tmp2 = sqrt(pow(second_havyX - second->arc[1].x, 2) + pow(second_havyY - second->arc[1].y, 2));
+      tmp3 = sqrt(pow(second_havyX - second->arc[2].x, 2) + pow(second_havyY - second->arc[2].y, 2));
 
-
-      second_R = max(tmp1,max(tmp2,tmp3));
-    }
-    else if(second->ID == "Trapeze"){
+      second_R = max(tmp1, max(tmp2, tmp3));
+    } else if (second->ID == "Trapeze") {
       second_havyX = (second->arc[0].x + second->arc[1].x + second->arc[2].x + second->arc[3].x) / 4;
       second_havyY = (second->arc[0].y + second->arc[1].y + second->arc[2].y + second->arc[3].y) / 4;
 
+      tmp1 = sqrt(pow(second_havyX - second->arc[0].x, 2) + pow(second_havyY - second->arc[0].y, 2));
+      tmp2 = sqrt(pow(second_havyX - second->arc[1].x, 2) + pow(second_havyY - second->arc[1].y, 2));
+      tmp3 = sqrt(pow(second_havyX - second->arc[2].x, 2) + pow(second_havyY - second->arc[2].y, 2));
+      tmp4 = sqrt(pow(second_havyX - second->arc[3].x, 2) + pow(second_havyY - second->arc[3].y, 2));
 
-      tmp1 = sqrt(pow(second_havyX - second->arc[0].x,2) + pow(second_havyY - second->arc[0].y,2));
-      tmp2 = sqrt(pow(second_havyX - second->arc[1].x,2) + pow(second_havyY - second->arc[1].y,2));
-      tmp3 = sqrt(pow(second_havyX - second->arc[2].x,2) + pow(second_havyY - second->arc[2].y,2));
-      tmp4 = sqrt(pow(second_havyX - second->arc[3].x,2) + pow(second_havyY - second->arc[3].y,2));
-
-
-      second_R = max(tmp1,max(tmp2,max(tmp3,tmp4)));
+      second_R = max(tmp1, max(tmp2, max(tmp3, tmp4)));
     }
 
-    double len = sqrt(pow(first_havyX - second_havyX,2) + pow(first_havyY - second_havyY,2));
+    double len = sqrt(pow(first_havyX - second_havyX, 2) + pow(first_havyY - second_havyY, 2));
 
-    if(len > (first_R + second_R)){
+    if (len > (first_R + second_R)) {
       return 1;
-    }
-    else if(len == (first_R + second_R)){
+    } else if (len == (first_R + second_R)) {
       return 2;
-    }
-    else{
+    } else {
       return 3;
     }
   }
-
-  static bool isInclude(Shape* first, Shape* second){
+  static long isInclude(Shape* first, Shape* second){
 
   }
 };
@@ -285,7 +269,44 @@ int main() {
         break;
       }
       case 3: {
-        cout << 2 << endl;
+        Shape *s1 = nullptr;
+        Shape *s2 = nullptr;
+        cout << "Для выполнения данной опции необходимо создать два объекта!" << endl;
+        cout << "=================================================================" << endl;
+        cout << "Выберите фигуру, которую необходимо создать:" << endl;
+        cout << "1) Треугольник" << endl;
+        cout << "2) Трапеция" << endl;
+        short int ch;
+        cin >> ch;
+        cout << "=================================================================" << endl;
+        s1 = Factory::createShape(ch);
+        cout << "=================================================================" << endl;
+        cout << "Выберите фигуру, которую необходимо создать:" << endl;
+        cout << "1) Треугольник" << endl;
+        cout << "2) Трапеция" << endl;
+        cin >> ch;
+        cout << "=================================================================" << endl;
+        s2 = Factory::createShape(ch);
+
+
+        long d = Operation::isInclude(s1,s2);
+        if(d == 1){
+          cout << "Фигуры совпадают" << endl;
+        }
+        else if(d == 2){
+          cout << "Первая фигура включена во вторую" << endl;
+        }
+        else if(d == 3){
+          cout << "Вторая фигура влючена в первую" << endl;
+        }
+        else if(d == 4){
+          cout << "Включение отсутсвует!" << endl;
+        }
+
+
+
+        delete s1;
+        delete s2;
         break;
       }
       default:
